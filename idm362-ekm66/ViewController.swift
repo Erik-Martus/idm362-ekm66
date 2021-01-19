946500d20bffe9e2a468d53f8f2975c40186f8f8
Erik-Martus <ekm66@drexel.edu>
2021-01-19 10:16:59 -0500

    Initial Commit

---
+++ idm362-ekm66/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  idm362-ekm66
+//
+//  Created by Erik Martus on 1/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ idm362-ekm66Tests/idm362_ekm66Tests.swift
@@ -0,0 +1,33 @@
+//
+//  idm362_ekm66Tests.swift
+//  idm362-ekm66Tests
+//
+//  Created by Erik Martus on 1/19/21.
+//
+
+import XCTest
+@testable import idm362_ekm66
+
+class idm362_ekm66Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
