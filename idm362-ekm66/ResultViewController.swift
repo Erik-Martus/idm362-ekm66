3d86db0307fe24a70cd89d7cd491544289ee1972
Erik-Martus <ekm66@drexel.edu>
2021-03-01 23:30:41 -0500

    Add toggle functionality to buttons

---
+++ idm362-ekm66/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  idm362-ekm66
+//
+//  Created by Erik Martus on 3/1/21.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- idm362-ekm66/ViewController.swift
+++ idm362-ekm66/ViewController.swift
@@ -9,11 +9,119 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Declare cuisine option buttons
+    @IBOutlet weak var OptionAmerican: UIButton!
+    @IBOutlet weak var OptionChinese: UIButton!
+    @IBOutlet weak var OptionFrench: UIButton!
+    @IBOutlet weak var OptionGreek: UIButton!
+    @IBOutlet weak var OptionIndian: UIButton!
+    @IBOutlet weak var OptionItalian: UIButton!
+    @IBOutlet weak var OptionJapanese: UIButton!
+    @IBOutlet weak var OptionMexican: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do after load.
+        
+        // Set initial background color for buttons
+        OptionAmerican.backgroundColor = .clear
+        OptionChinese.backgroundColor = .clear
+        OptionFrench.backgroundColor = .clear
+        OptionGreek.backgroundColor = .clear
+        OptionIndian.backgroundColor = .clear
+        OptionItalian.backgroundColor = .clear
+        OptionJapanese.backgroundColor = .clear
+        OptionMexican.backgroundColor = .clear
+    } // End viewDidLoad
+
+    @IBAction func TapAmerican(_ sender: Any) {
+        if OptionAmerican.backgroundColor == .clear {
+            OptionAmerican.backgroundColor = UIColor(named: "AccentColor")
+            OptionAmerican.setTitleColor(UIColor.white, for: .normal)
+        }
+        else if OptionAmerican.backgroundColor == UIColor(named: "AccentColor") {
+            OptionAmerican.backgroundColor = .clear
+            OptionAmerican.setTitleColor(UIColor.label, for: .normal)
+        }
     }
-
-
-}
+    
+    @IBAction func TapChinese(_ sender: Any) {
+        if OptionChinese.backgroundColor == .clear {
+            OptionChinese.backgroundColor = UIColor(named: "AccentColor")
+            OptionChinese.setTitleColor(UIColor.white, for: .normal)
+        }
+        else if OptionChinese.backgroundColor == UIColor(named: "AccentColor") {
+            OptionChinese.backgroundColor = .clear
+            OptionChinese.setTitleColor(UIColor.label, for: .normal)
+        }
+    }
+    
+    @IBAction func TapFrench(_ sender: Any) {
+        if OptionFrench.backgroundColor == .clear {
+            OptionFrench.backgroundColor = UIColor(named: "AccentColor")
+            OptionFrench.setTitleColor(UIColor.white, for: .normal)
+        }
+        else if OptionFrench.backgroundColor == UIColor(named: "AccentColor") {
+            OptionFrench.backgroundColor = .clear
+            OptionFrench.setTitleColor(UIColor.label, for: .normal)
+        }
+    }
+    
+    @IBAction func TapGreek(_ sender: Any) {
+        if OptionGreek.backgroundColor == .clear {
+            OptionGreek.backgroundColor = UIColor(named: "AccentColor")
+            OptionGreek.setTitleColor(UIColor.white, for: .normal)
+        }
+        else if OptionGreek.backgroundColor == UIColor(named: "AccentColor") {
+            OptionGreek.backgroundColor = .clear
+            OptionGreek.setTitleColor(UIColor.label, for: .normal)
+        }
+    }
+    
+    
+    @IBAction func TapIndian(_ sender: Any) {
+        if OptionIndian.backgroundColor == .clear {
+            OptionIndian.backgroundColor = UIColor(named: "AccentColor")
+            OptionIndian.setTitleColor(UIColor.white, for: .normal)
+        }
+        else if OptionIndian.backgroundColor == UIColor(named: "AccentColor") {
+            OptionIndian.backgroundColor = .clear
+            OptionIndian.setTitleColor(UIColor.label, for: .normal)
+        }
+    }
+    
+    @IBAction func TapItalian(_ sender: Any) {
+        if OptionItalian.backgroundColor == .clear {
+            OptionItalian.backgroundColor = UIColor(named: "AccentColor")
+            OptionItalian.setTitleColor(UIColor.white, for: .normal)
+        }
+        else if OptionItalian.backgroundColor == UIColor(named: "AccentColor") {
+            OptionItalian.backgroundColor = .clear
+            OptionItalian.setTitleColor(UIColor.label, for: .normal)
+        }
+    }
+    
+    @IBAction func TapJapanese(_ sender: Any) {
+        if OptionJapanese.backgroundColor == .clear {
+            OptionJapanese.backgroundColor = UIColor(named: "AccentColor")
+            OptionJapanese.setTitleColor(UIColor.white, for: .normal)
+        }
+        else if OptionJapanese.backgroundColor == UIColor(named: "AccentColor") {
+            OptionJapanese.backgroundColor = .clear
+            OptionJapanese.setTitleColor(UIColor.label, for: .normal)
+        }
+    }
+    
+    @IBAction func TapMexican(_ sender: Any) {
+        if OptionMexican.backgroundColor == .clear {
+            OptionMexican.backgroundColor = UIColor(named: "AccentColor")
+            OptionMexican.setTitleColor(UIColor.white, for: .normal)
+        }
+        else if OptionMexican.backgroundColor == UIColor(named: "AccentColor") {
+            OptionMexican.backgroundColor = .clear
+            OptionMexican.setTitleColor(UIColor.label, for: .normal)
+        }
+    }
+    
+} // End ViewController
 
